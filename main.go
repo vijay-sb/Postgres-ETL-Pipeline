a657e51e42a276963d0cf5d916916eea8b3e0754
vijaysb0613 <vijaysb2006@gmail.com>
2025-09-20 15:48:43 +0530

    feat:Read csv file (test data)

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
 
+// Reading the csv test files
+func readCsv(filepath string) [][]string {
+	f, error := os.Open(filepath)
+	if error != nil {
+		log.Fatal("Unable to read the csv file")
+	}
+	defer f.Close()
+
+	csvReader := csv.NewReader(f)
+	recors, err := csvReader.ReadAll()
+
+	if err != nil {
+		log.Fatal("Unable to parse the csv file")
+	}
+
+	return recors
+}
 func main() {
-  fmt.Println("Hello");
+	fmt.Println("Hello")
+	csvContent := readCsv("test_data/taxi_zone_geo.csv")
+	fmt.Println(csvContent)
 }
